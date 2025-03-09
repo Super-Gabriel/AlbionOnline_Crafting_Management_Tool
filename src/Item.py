f60b3ec939f46c1a761390dc1f9b64f00611ed7b
m4ndarina <mariangc2701@gmail.com>
2025-03-08 23:12:23 -0500

    Setters y getters de la clase item agregados

--- src/Item.py
+++ src/Item.py
@@ -1,3 +1,37 @@
 # archivo para implementar la clase item
 class Item:
-    pass
+    def __init__(self):
+        self.alias = ""
+        self.item_id = ""
+        self.item_qty = 0
+        self.rsrc = {}
+    
+    # Definir el nombre del objeto
+    def setAlias(self, alias):
+        self.alias = alias
+
+     # Definir el ID del objeto
+    def setItem_id(self, item_id):
+        self.item_id = item_id
+    
+     # Definir la cantidad a craftear del objeto
+    def setItem_qty(self, item_qty):
+        self.item_qty = item_qty
+    
+     # Definir los recursos 
+    def setRsrc(self, rsrc):
+        self.rsrc = rsrc
+    
+    # - - - - - - - - - - - - - - - - - - - - - - - - 
+    
+    def getAlias(self):
+        return self.alias
+
+    def getItem_id(self):
+        return self.item_id
+
+    def getItem_qty(self):
+        return self.item_qty
+
+    def getRsrc(self):
+        return self.rsrc
