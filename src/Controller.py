266117c5d641eb13db412db6782ef6ae6f471c83
m4ndarina <mariangc2701@gmail.com>
2025-03-13 15:33:16 -0500

    pruebas main algorithm

--- src/Controller.py
+++ src/Controller.py
@@ -10,6 +10,27 @@ class Controller:
         self.cities: str
         self.quality: int
 
+    def main_algorithm(self):
+        buy_rsrc = 60
+        avg_p_rsrc = self.get_avg_price("T5_METALBAR_LEVEL1@1")
+
+        rsrc_expense = self.calc_mng.get_expense(buy_rsrc, avg_p_rsrc)
+        print(f"Precio por los recursos: {rsrc_expense}")
+
+        # ----------------------------------------------------------------
+        
+        buy_obj = 30
+        avg_p_obj = self.get_avg_price("T5_MAIN_AXE@1")
+
+        obj_earnings = self.calc_mng.get_expense(buy_obj, avg_p_obj)
+        print(f"Precio por los objetos: {obj_earnings}")
+
+        # ----------------------------------------------------------------
+        
+        total_profit = self.calc_mng.get_profit(rsrc_expense, obj_earnings)
+        print(f"Profit al craftear: {total_profit}")
+        
+
     # metodo para obtener el precio promedio dado el id de un objeto
     def get_avg_price(self, obj_id: str) -> float:
         lista = []
