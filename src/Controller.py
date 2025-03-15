fdd64dba7f3d5e48f66e2d396a6375c3bde360c1
Gabriel Sánchez <gabriel002002@gmail.com>
2025-03-14 21:15:35 -0600

    versión 2 de proyecto con logs incluidos

--- src/Controller.py
+++ src/Controller.py
@@ -10,6 +10,7 @@ class Controller:
         self.cities: str
         self.quality: int
         self.p: float
+        self.logs = []
 
     def main_algorithm(self):
         profits = []
@@ -21,6 +22,7 @@ class Controller:
             item_name = item.get_alias()
             profits.append([item_name, item_profit])
 
+        print(self.logs)
         print(profits)
 
     # metodo para calcular el profit de un item especifico
@@ -33,12 +35,18 @@ class Controller:
         for rsrc_id in rsrc.keys(): # construyendo la lista de precios promedio de los recursos
             avg_rsrc_prices.append(self.get_avg_price(rsrc_id))
 
-        expense_list = [ x * y for x, y in zip(total_rsrc, avg_rsrc_prices)]
+        expense_list = [ x * y for x, y in zip(total_rsrc, avg_rsrc_prices)] 
         total_expenses = sum(expense_list)
         #__________________________________________________________________________________
-        earnings = self.get_avg_price(item_id) * qty
+        item_price = self.get_avg_price(item_id)
+        earnings = item_price * qty
         profit = earnings - total_expenses
 
+        rsrc_ids = self.get_dict_keys(rsrc)
+
+        log_dict = self.build_log_dict(item_id, qty, item_price, rsrc_ids, total_rsrc, avg_rsrc_prices, total_expenses, earnings, profit)
+        self.logs.append(log_dict)
+        
         return profit
 
     # metodo para obtener el precio promedio dado el id de un objeto
@@ -49,7 +57,7 @@ class Controller:
             min_price = item.get("sell_price_min")
             max_price = item.get("sell_price_max")
             lista.append(min_price)
-            lista.append(max_price)        
+            lista.append(max_price)
         #print(lista)
         lista = self.calc_mng.substract_min(lista)
         lista = self.calc_mng.substract_max(lista)
@@ -64,6 +72,43 @@ class Controller:
             return self.calc_mng.calculate_rsrc(n_objects, rsrc_qty, p)
         return n_objects
 
+    # Método para construir un log_dict
+    def build_log_dict(self, 
+                       item_id: str, 
+                       qty: int, 
+                       item_price: float, 
+                       rsrc_ids: list, 
+                       total_rsrc: list, 
+                       avg_rsrc_prices: list, 
+                       total_expenses: float, 
+                       earnings: float, 
+                       profit: float) -> dict:
+        
+        rsrc_info_dict = {}
+        contador = 1
+        for id, total_rsrc, avg_rsrc_price in zip(rsrc_ids, total_rsrc, avg_rsrc_prices):
+            rsrc_info_dict[f'rsrc{contador}_name'] = id
+            rsrc_info_dict[f'rsrc{contador}_qty'] = total_rsrc
+            rsrc_info_dict[f'rsrc{contador}_avg_price'] = avg_rsrc_price
+            contador += 1
+
+        result = {}
+        result['item_name'] = item_id
+        result['item_qty'] = qty
+        result['item_avg_price'] = item_price
+        result['rsrc_info'] = rsrc_info_dict
+        result['total_expenses'] = total_expenses
+        result['earnings'] = earnings
+        result['profit'] = profit
+        
+        return result
+    
+    # Método para obtener las keys de un dict
+    def get_dict_keys(self, dct: dict) -> list:
+        result = []
+        for key in dct.keys():
+            result.append(key)
+        return result
 
     ## setters _______________________________________________________________________
     def set_item_list(self, item_list: list):
