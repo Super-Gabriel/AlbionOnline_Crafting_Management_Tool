d298e069ac949b3de9821ba5ed02cfab625e26de
Gabriel Sánchez <gabriel002002@gmail.com>
2025-03-15 19:39:18 -0600

    guardado de logs agregado

--- src/ApiConnection.py
+++ src/ApiConnection.py
@@ -1,14 +1,10 @@
 import requests
+import pandas as pd
 
 class ApiConnection:
     def __init__(self):
         self.url: str
-
-    # metodo para mandar una solicitud a un api
-    def send_request_1(self, url: str): 
-        print("enviando solicitud")
-        print(url)
-        
+        self.df = pd.DataFrame(columns=["id", "city", "quality", "s_p_min", "s_p_max"])
 
     # metodo para hacer una request dado un id, ciudades y calidad, regresa un dict con la respuesta de la api
     def send_request(self, id: str, cities: str, quality: int) -> dict[dict]:
@@ -21,9 +17,28 @@ class ApiConnection:
             reversed(datos) # invirtiendo datos
         except Exception as e:
             print(f"ocurrió un error al hacer la solicitud :c {e}")
+
+        self.save_data_request(datos)
+
         return datos
+    
+    # metodo para imprimir una lista de dicts
+    def save_data_request(self, list):
+        for item in list:
+            self.insert_row(item)
+
+    # Metodo para imprimir un diccionario en bonito
+    def insert_row(self, dct: dict):
+        new_row = [dct["item_id"],dct["city"], dct["quality"], dct["sell_price_min"], dct["sell_price_max"]]
+        self.df.loc[len(self.df)] = new_row
 
     ## setters______________________________________________________________________________________________
 
     def set_url(self, id: str, cities: str, quality: int):
         self.url = f"https://west.albion-online-data.com/api/v2/stats/prices/{id}?locations={cities},&qualities={quality}"
+
+
+    ## getters______________________________________________________________________________________________
+
+    def get_df_log(self):
+        return self.df
